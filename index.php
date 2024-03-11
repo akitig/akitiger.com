3583efd80f416e1b87e8fe2147b853f0fe89a00a
akitig <akitig24@gmail.com>
2024-03-11 18:39:22 +0900

    update About me

--- index.php
+++ index.php
@@ -17,10 +17,36 @@
 
 <body>
     <header>
-        <h1 id="title">akitig portfolio</h1>
+        <h1 id="top-title">akitig portfolio</h1>
     </header>
     <main>
-        <p>helloworld</p>
+        <div class="container">
+            <div class="box">
+                <h2 id="chap-title">about me</h2>
+                <div class="content">
+                    <div class="aboutme">
+                        <div class="item">
+                            <div class="rectangle"></div>
+                        </div>
+                        <div class="item">
+                            <p class="profile">2026年3月大学卒業予定<br><br>
+                                相手が発信してくれた「やりたい」を、全力で叶えたい<br>
+                                その情熱を忘れず、叶えるための技術力を身につけたい<br>
+                                この気持ちを大切に、毎日勉強と制作を頑張っています<br>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="box">
+                <h2 id="chap-title">works</h2>
+                <div class="rectangle"></div>
+            </div>
+            <div class="box">
+                <h2 id="chap-title">skill</h2>
+                <div class="rectangle"></div>
+            </div>
+        </div>
     </main>
     <footer>
         <p id="copyright">&copy; akitig portfolio</p>
