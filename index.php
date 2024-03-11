6329d71ba09de9836c7395ed1e9c76042f5e5b88
akitig <akitig24@gmail.com>
2024-03-11 17:16:01 +0900

    add header footer

--- index.php
+++ index.php
@@ -5,13 +5,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat+Subrayada:wght@400;700&display=swap" rel="stylesheet">
+
     <link rel="stylesheet" href=style.css>
     <link rel="shortcut icon" href="images/favicon.ico">
-    <title>Hello World</title>
+    <title>akitig</title>
 </head>
 
 <body>
-    <p>helloworld</p>
+    <header>
+        <h1 id="title">akitig portfolio</h1>
+    </header>
+    <main>
+        <p>helloworld</p>
+    </main>
+    <footer>
+        <p id="copyright">&copy; akitig portfolio</p>
+    </footer>
 </body>
+
 
 </html>
