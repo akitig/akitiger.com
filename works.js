b80ce2cc73e658aab0871e82de433172a0335506
akitig <akitig24@gmail.com>
2024-03-14 17:20:36 +0900

    works add slideshow

---
+++ works.js
@@ -0,0 +1,10 @@
+$(function () {
+    $(".test-slick").slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: true,
+        dots:true,
+        centerMode:true,
+        centerPadding:'100px'
+    });
+});
